5521903851daeb7ade86088ee4fed355c9ec8e6d
dongyeop <brassdong@gmail.com>
2022-03-17 22:03:31 +0900

    DFS 백트래킹
    깊이우선서치

---
+++ backtracking/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb= new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        sc.close();
+
+
+
+        arr=new int[M];
+        visit=new boolean[N];
+
+        dfs(N,M,0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int N, int M, int depth){
+        if(depth==M){
+            for(int num:arr){
+                sb.append(num).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=0;i<N;i++){
+            if(!visit[i]){
+                visit[i]=true;
+                arr[depth]=i+1;
+                dfs(N,M,depth+1);
+                visit[i]=false;
+            }
+        }
+
+    }
+
+}
