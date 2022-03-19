eb36a3d6572d7bb0d7bfc68e2e700ffa88fa6ab7
dongyeop <brassdong@gmail.com>
2022-03-19 23:22:55 +0900

    DFS operation

--- backtracking/14888/src/Main.java
+++ backtracking/14888/src/Main.java
@@ -2,13 +2,14 @@ import java.util.*;
 public class Main {
 
     public static int[] arr;
-    public static int max=0;
-    public static int min=0;
+    public static int max=-1000000000;
+    public static int min=1000000000;
     public static int[] operation= new int[4];
     public static int opNum;
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        StringBuilder sb=new StringBuilder();
         int N=sc.nextInt();
         arr= new int[N];
         opNum=N-1;
@@ -17,7 +18,9 @@ public class Main {
         for(int i=0;i<4;i++)
             operation[i] = sc.nextInt();
         sc.close();
-        dfs(0,0,0,0,0);
+        dfs(0,0,0,0,arr[0]);
+        sb.append(max).append("\n").append(min);
+        System.out.println(sb);
 
     }
 
@@ -31,7 +34,18 @@ public class Main {
         }
 
         for(int i=0;i<opNum;i++){
-            
+            if(plus<operation[0]){
+                dfs(plus+1,minus,mul,div,num+arr[i+1]);
+            }
+            if(minus<operation[1]){
+                dfs(plus,minus+1,mul,div,num-arr[i+1]);
+            }
+            if(mul<operation[2]){
+                dfs(plus,minus,mul+1,div,num*arr[i+1]);
+            }
+            if(plus<operation[3]){
+                dfs(plus,minus,mul,div+1,num/arr[i+1]);
+            }
         }
     }
 
