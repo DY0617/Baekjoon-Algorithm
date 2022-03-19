bd8a748fb58d8eba4b714351072823a7426e6b32
dongyeop <brassdong@gmail.com>
2022-03-20 00:22:39 +0900

    DFS operater

--- backtracking/14888/src/Main.java
+++ backtracking/14888/src/Main.java
@@ -1,51 +1,51 @@
 import java.util.*;
+
 public class Main {
 
     public static int[] arr;
-    public static int max=-1000000000;
-    public static int min=1000000000;
-    public static int[] operation= new int[4];
+    public static int max=Integer.MIN_VALUE;
+    public static int min=Integer.MAX_VALUE;
+    public static int[] operation = new int[4];
     public static int opNum;
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        StringBuilder sb=new StringBuilder();
-        int N=sc.nextInt();
-        arr= new int[N];
-        opNum=N-1;
-        for(int i=0;i<N;i++)
-            arr[i]=sc.nextInt();
-        for(int i=0;i<4;i++)
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt();
+        arr = new int[N];
+        opNum = N - 1;
+        for (int i = 0; i < N; i++)
+            arr[i] = sc.nextInt();
+        for (int i = 0; i < 4; i++)
             operation[i] = sc.nextInt();
         sc.close();
-        dfs(0,0,0,0,arr[0]);
+        dfs(0, arr[0]);
         sb.append(max).append("\n").append(min);
         System.out.println(sb);
 
     }
 
-    public static void dfs(int plus,int minus, int mul, int div,int num){
-        if(opNum==(plus+minus+mul+div)){
-            if(num>max)
-                max=num;
-            if(num<min)
-                min=num;
+    public static void dfs(int index, int num) {
+        if (opNum == index) {
+            max=Math.max(num,max);
+            min=Math.min(num,min);
             return;
         }
 
-        for(int i=0;i<opNum;i++){
-            if(plus<operation[0]){
-                dfs(plus+1,minus,mul,div,num+arr[i+1]);
+        for (int i = 0; i < 4; i++) {
+            if(operation[i]>0){
+                operation[i]--;
+                if (i == 0)
+                    dfs(index+1,num+arr[index+1]);
+                if (i == 1)
+                    dfs(index+1,num-arr[index+1]);
+                if (i == 2)
+                    dfs(index+1,num*arr[index+1]);
+                if (i == 3)
+                    dfs(index+1,num/arr[index+1]);
+                operation[i]++;
             }
-            if(minus<operation[1]){
-                dfs(plus,minus+1,mul,div,num-arr[i+1]);
-            }
-            if(mul<operation[2]){
-                dfs(plus,minus,mul+1,div,num*arr[i+1]);
-            }
-            if(plus<operation[3]){
-                dfs(plus,minus,mul,div+1,num/arr[i+1]);
-            }
+
         }
     }
 
