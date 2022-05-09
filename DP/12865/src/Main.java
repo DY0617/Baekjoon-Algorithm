e4e32e2c0c8c6e416eeac8871e3a10235bae97e8
dongyeop <brassdong@gmail.com>
2022-05-09 19:37:22 +0900

    Update Main.java

--- DP/12865/src/Main.java
+++ DP/12865/src/Main.java
@@ -3,7 +3,22 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static int N, K, W, V;
+    static int[] dp;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dp=new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            W = Integer.parseInt(st.nextToken());
+            V = Integer.parseInt(st.nextToken());
+        }
     }
 }
