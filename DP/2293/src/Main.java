2e3bb23a95a1d38a40bb679e9a69bbcb97212cad
dongyeop <brassdong@gmail.com>
2022-05-09 23:25:12 +0900

    dp 동전

--- DP/2293/src/Main.java
+++ DP/2293/src/Main.java
@@ -13,16 +13,24 @@ public class Main {
         n= Integer.parseInt(st.nextToken());
         k= Integer.parseInt(st.nextToken());
 
-        arr=new int[n];
-        dp=new int[k];
+        arr=new int[n+1];
+        dp=new int[k+1];
 
-        for(int i=0;i<n;i++){
+
+        for(int i=1;i<=n;i++){
             st=new StringTokenizer(br.readLine());
             arr[i]=Integer.parseInt(st.nextToken());
-            for (int j = arr[i]; j < k; j++)
-                dp[j] += dp[j - arr[i]];
         }
 
-        System.out.println(dp[k-1]);
+        dp[0]=1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = arr[i]; j <= k; j++) {
+                dp[j] = dp[j] + dp[j - arr[i]];
+            }
+        }
+
+
+        System.out.println(dp[k]);
     }
 }
