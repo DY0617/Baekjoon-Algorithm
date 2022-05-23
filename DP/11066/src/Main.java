306ad20aaa220fe682d59186eebc283970759205
dongyeop <brassdong@gmail.com>
2022-05-23 21:26:51 +0900

    11066

--- DP/11066/src/Main.java
+++ DP/11066/src/Main.java
@@ -3,7 +3,8 @@ import java.io.*;
 
 public class Main {
     static int t,k;
-    static int[] arr;
+    static int[] arr,sum;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -13,7 +14,9 @@ public class Main {
         for(int i=0;i<t;i++) {
             st = new StringTokenizer(br.readLine());
             k = Integer.parseInt(st.nextToken());
-            arr=new int[k];
+            arr=new int[k+1];
+            sum=new int[k+1];
+            dp=new int[k+1][k+1];
             st = new StringTokenizer(br.readLine());
             for(int j=0;j<k;j++){
                 arr[j]=Integer.parseInt(st.nextToken());
