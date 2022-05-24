522621e9f3dba3c84ccdda90ac9594b0e6daf99c
dongyeop <brassdong@gmail.com>
2022-05-24 20:47:49 +0900

    dp

---
+++ DP/11049/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter vw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException{
+
+    }
+}
