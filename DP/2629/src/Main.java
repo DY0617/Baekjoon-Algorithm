a2a03b8d39f187b8a76996397dc5495985606f27
dongyeop <brassdong@gmail.com>
2022-07-25 13:13:29 +0900

    Update Main.java

--- DP/2629/src/Main.java
+++ DP/2629/src/Main.java
@@ -2,9 +2,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
 
-    public static void main(String[] args) {
+    static int N1,N2;//N1=추의 개수, N2=구슬의 개수
+    static int[] arr1,arr2;
+    static int[][] dp;
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException{
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        N1= Integer.parseInt(st.nextToken());
+        arr1=new int[N1];
+
+        st=new StringTokenizer(br.readLine());
+
+        for(int i=0;i<N1;i++){
+            arr1[i]=Integer.parseInt(st.nextToken());
+        }
+
+        st=new StringTokenizer(br.readLine());
+        N2= Integer.parseInt(st.nextToken());
+        arr2=new int[N2];
+
+        st=new StringTokenizer(br.readLine());
+
+        for(int i=0;i<N2;i++){
+            arr2[i]=Integer.parseInt(st.nextToken());
+        }
 
     }
 }
