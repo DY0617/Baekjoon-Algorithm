0d81fe5c1096ec964b0b4ad9e493f3135d02175b
dongyeop <brassdong@gmail.com>
2022-07-27 23:15:46 +0900

    수정중

--- DP/7579/src/Main.java
+++ DP/7579/src/Main.java
@@ -4,11 +4,30 @@ import java.io.*;
 
 public class Main {
 
-    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-    static int N,M;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N, M;
+    static int[][] arr;
+    static int[][] dp;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr=new int[N][2];
+        dp=new int[N][N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i][0]=Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i][1]=Integer.parseInt(st.nextToken());
+        }
 
     }
 }
