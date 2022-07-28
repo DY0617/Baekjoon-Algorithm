ff04ef1a40b814b17eb601d28b37f757e89c5d93
dongyeop <brassdong@gmail.com>
2022-07-28 12:24:00 +0900

    dp

--- DP/7579/src/Main.java
+++ DP/7579/src/Main.java
@@ -5,10 +5,9 @@ import java.io.*;
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N, M;
     static int[][] arr;
-    static int[][] dp;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
 
@@ -16,19 +15,42 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr=new int[N+1][2];
-        dp=new int[N+1][10001];
+        arr=new int[N][2];
+        dp=new int[10001];
 
         st = new StringTokenizer(br.readLine());
-        for(int i=1;i<=N;i++){
+        for(int i=0;i<N;i++){
             arr[i][0]=Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
-        for(int i=1;i<=N;i++){
+        for(int i=0;i<N;i++){
             arr[i][1]=Integer.parseInt(st.nextToken());
         }
         Arrays.fill(dp,-1);
+
+        for(int i=0;i<N;i++){
+            int cost=arr[i][1];
+
+            for(int j=10000;j>=cost;j--){
+                if(dp[j-cost]!=-1){
+                    if(dp[j-cost]+arr[i][0]>dp[j]){
+                        dp[j]=dp[j-cost]+arr[i][0];
+                    }
+                }
+            }
+
+            if(dp[cost]<arr[i][0])
+                dp[cost]=arr[i][0];
+
+        }
+
+        for(int i=0;i<=10000;i++){
+            if(dp[i]>=M){
+                System.out.println(i);
+                break;
+            }
+        }
 
 
     }
