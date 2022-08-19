d4394c78c7e15c96ad9b17beaeb6157b8a68caa5
dongyeop <brassdong@gmail.com>
2022-08-19 21:18:40 +0900

    스택

--- Stack/17298/src/Main.java
+++ Stack/17298/src/Main.java
@@ -5,14 +5,37 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
         Stack<Integer> stack=new Stack<>();
+        int[] arr;
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
         int N= Integer.parseInt(st.nextToken());
+        arr=new int[N];
         st=new StringTokenizer(br.readLine());
 
         for(int i=0;i<N;i++){
-            stack.push(Integer.parseInt(st.nextToken()));
+            arr[i]=Integer.parseInt(st.nextToken());
         }
+
+        for(int i=0;i<N;i++){
+
+            while(!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(' ');
+        }
+
+        System.out.println(sb);
+
+        br.close();
 
     }
 }
