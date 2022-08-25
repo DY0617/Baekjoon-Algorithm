4ac21464112e2b3f28f629c729a66e920eb689bb
dongyeop <brassdong@gmail.com>
2022-08-25 20:41:28 +0900

    tree diameter

---
+++ Tree/1967/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        int N= Integer.parseInt(st.nextToken());
+
+        for(int i=0;i<N;i++){
+            st=new StringTokenizer(br.readLine());
+        }
+    }
+}
