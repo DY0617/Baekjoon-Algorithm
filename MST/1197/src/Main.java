f1d87558a491a0f02741a3df885266d68fdfd48d
dongyeop <brassdong@gmail.com>
2022-10-31 15:51:34 +0900

    mst

--- MST/1197/src/Main.java
+++ MST/1197/src/Main.java
@@ -4,10 +4,37 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st=new StringTokenizer(br.readLine());
 
-        
+        class Node{
+            int b,cost;
+            Node(int b, int cost){
+                this.b=b;
+                this.cost=cost;
+            }
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        ArrayList<Node>[] graph = new ArrayList[v];
+
+        for (int i = 0; i < v; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph[a].add(new Node(b,c));
+            graph[b].add(new Node(a,c));
+        }
+
+
     }
 }
