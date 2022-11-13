50ce5fc44ebbe0b5c6774da25095df7e8c6ba682
dongyeop <brassdong@gmail.com>
2022-11-13 19:05:02 +0900

    mst

--- MST/2887/src/Main.java
+++ MST/2887/src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
@@ -7,12 +8,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         class Node {
-            int x, y, z;
+            int x, y, z, index;
 
-            Node(int a, int b, int c) {
+            Node(int a, int b, int c, int index) {
                 x = a;
                 y = b;
                 z = c;
+                this.index = index;
             }
         }
 
@@ -44,19 +46,44 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            planet.add(new Node(a, b, c));
+            planet.add(new Node(a, b, c, i + 1));
         }
+
+        ArrayList<Node> sortX = new ArrayList<Node>(planet);
+        Collections.sort(sortX, new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.x - o2.x;
+            }
+        });
+        ArrayList<Node> sortY = new ArrayList<Node>(planet);
+        Collections.sort(sortY, new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.y - o2.y;
+            }
+        });
+        ArrayList<Node> sortZ = new ArrayList<Node>(planet);
+        Collections.sort(sortZ, new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.z - o2.z;
+            }
+        });
+
 
         PriorityQueue<NodeW> pq = new PriorityQueue<>();
 
-        for (int i = 0; i < n; i++) {
-            Node n1 = planet.get(i);
-            for (int j = i + 1; j < n; j++) {
-                Node n2 = planet.get(j);
-                int cost = Math.min(Math.abs(n1.x - n2.x), Math.abs(n1.y - n2.y));
-                cost = Math.min(cost, Math.abs(n1.z - n2.z));
-                pq.add(new NodeW(i + 1, j + 1, cost));
-            }
+        for (int i = 0; i < n - 1; i++) {
+            Node p1 = sortX.get(i);
+            Node p2 = sortX.get(i + 1);
+            pq.add(new NodeW(p1.index, p2.index, Math.min(Math.abs(p1.x - p2.x), Math.min(Math.abs(p1.y - p2.y), Math.abs(p1.z - p2.z)))));
+            p1 = sortY.get(i);
+            p2 = sortY.get(i + 1);
+            pq.add(new NodeW(p1.index, p2.index, Math.min(Math.abs(p1.x - p2.x), Math.min(Math.abs(p1.y - p2.y), Math.abs(p1.z - p2.z)))));
+            p1 = sortZ.get(i);
+            p2 = sortZ.get(i + 1);
+            pq.add(new NodeW(p1.index, p2.index, Math.min(Math.abs(p1.x - p2.x), Math.min(Math.abs(p1.y - p2.y), Math.abs(p1.z - p2.z)))));
         }
 
         parent = new int[n + 1];
@@ -64,17 +91,20 @@ public class Main {
             parent[i] = i;
         }
 
-        int answer=0;
+        int answer = 0;
 
-        int size=pq.size();
+        int size = pq.size();
 
-        for(int i=0;i<size;i++){
-            NodeW node=pq.poll();
-            int x=find(node.start);
-            int y=find(node.end);
-            if(x!=y){
-                answer+=node.cost;
-                parent[y]=x;
+        for (int i = 0; i < size; i++) {
+            NodeW node = pq.poll();
+            int x = find(node.start);
+            int y = find(node.end);
+            if (x != y) {
+                if (x > y)
+                    parent[x] = y;
+                else
+                    parent[y] = x;
+                answer += node.cost;
             }
         }
 
@@ -86,9 +116,9 @@ public class Main {
 
     }
 
-    public static int find(int x){
-        if(parent[x]==x)
+    public static int find(int x) {
+        if (parent[x] == x)
             return x;
-        return parent[x]=find(parent[x]);
+        return parent[x] = find(parent[x]);
     }
 }
